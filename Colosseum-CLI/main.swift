9f42fbfb78e34e5d8df4402899455801d99efb6e
Stars Momodu <starsmomodu@gmail.com>
2020-10-20 01:43:31 +0100

    small multiplatform support

--- Colosseum-CLI/main.swift
+++ Colosseum-CLI/main.swift
@@ -7,5 +7,120 @@
 
 import Foundation
 
-XGFolders.setUpFolderFormat()
+func invalidOption(_ option: String) {
+	printg("Invalid function number:", option)
+}
 
+func listFiles() {
+	ISO.allFileNames.forEach {
+		printg($0)
+	}
+}
+
+func importExportFiles() {
+	var currentSearch = ""
+
+	func searchedFiles() -> [String] {
+		guard currentSearch.count > 0 else {
+			return ISO.allFileNames
+		}
+		return ISO.allFileNames.filter { (name) -> Bool in
+			return name.lowercased().contains(currentSearch.lowercased())
+		}
+	}
+
+	func exportFiles() {
+		for file in searchedFiles() {
+			let outputFile = XGFiles.nameAndFolder(file, .ISOExport(file.removeFileExtensions()))
+			printg("Exporting file", file, "to:", outputFile.folder.path)
+			if XGUtility.exportFileFromISO(outputFile) {
+				printg("success")
+			} else {
+				printg("failed")
+			}
+		}
+	}
+
+	func importFiles() {
+		for file in searchedFiles() {
+			let inputFile = XGFiles.nameAndFolder(file, .ISOExport(file.removeFileExtensions()))
+			printg("Importing file", file, "from:", inputFile.folder.path)
+			if XGUtility.importFileToISO(inputFile) {
+				printg("success")
+			} else {
+				printg("failed")
+			}
+		}
+	}
+
+	func list() {
+		searchedFiles().forEach { (file) in
+			printg(file)
+		}
+	}
+
+	while true {
+		let searchText = currentSearch.count == 0 ? "all files" : "all files containing `\(currentSearch)`"
+
+		let prompt = """
+		Enter 'export' to extract \(searchText)
+		Enter 'import' to import \(searchText)
+		Enter 'list' to list \(searchText)
+		Enter 'exit' to go back
+		Enter any other text to search for files containing that text
+		"""
+		let input = readInput(prompt)
+		switch input {
+		case "export": exportFiles()
+		case "import": importFiles()
+		case "list": list()
+		case "exit": return
+		default: currentSearch = input
+		}
+	}
+}
+
+func mainMenu() {
+	while true {
+		let prompt = """
+		Type the number of the function you want and press enter:
+
+		0: Exit the program
+		1: List files
+		2: Import/Export files
+		"""
+
+		let input = readInput(prompt)
+		switch input {
+		case "": continue
+		case "0": return
+		case "1": listFiles()
+		case "2": importExportFiles()
+		default: invalidOption(input)
+		}
+	}
+}
+
+func readInput(_ prompt: String) -> String {
+	print("\n" + prompt, terminator: "\n\n>> ")
+	return readLine() ?? ""
+}
+
+func main() {
+	let noDocumentsFolder = !XGFolders.Documents.exists
+	XGFolders.setUpFolderFormat()
+	if noDocumentsFolder {
+		printg("Created folder structure at:", XGFolders.Documents.path)
+	}
+	guard XGFiles.iso.exists else {
+		printg("Please place your iso in the folder", XGFolders.ISO.path, "and name it \"\(XGFiles.iso.fileName)\"")
+		printg("Once you've done this you can run the tool again. :-)")
+		return
+	}
+
+	mainMenu()
+}
+
+main()
+printg("Press enter to close the tool...")
+_ = readLine() // pause at end of execution
