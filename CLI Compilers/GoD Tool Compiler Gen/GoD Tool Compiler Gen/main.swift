5fd23ea245226ba377f255f6258649bca4e3a1fb
Stars Momodu <starsmomodu@gmail.com>
2020-10-23 01:52:53 +0100

    Randomiser in CLI

--- CLI Compilers/GoD Tool Compiler Gen/GoD Tool Compiler Gen/main.swift
+++ CLI Compilers/GoD Tool Compiler Gen/GoD Tool Compiler Gen/main.swift
@@ -124,7 +124,7 @@ var windowsCompiler = "set SWIFTFLAGS=-sdk %SDKROOT% -resource-dir %SDKROOT%\\us
 + "mkdir " + GoDAssetsFolder.replacingOccurrences(of: "/", with: "\\") + "\n"
 + "mkdir " + GoDWiimmsAssetsFolder.replacingOccurrences(of: "/", with: "\\") + "\n"
 
-// MARK: - Add copy XD assets to scripts
+// MARK: - Copy XD assets to scripts
 
 for asset in GoDAssets {
 	osxCompiler += "cp ../\(asset.path.replacingOccurrences(of: " ", with: "\\ ")) \(GoDAssetsFolder)/\(asset.filename.replacingOccurrences(of: " ", with: "\\ "))\n"
@@ -146,68 +146,99 @@ for file in GoDSources {
 	windowsCompiler += " \"..\\" + file.replacingOccurrences(of: "/", with: "\\") + "\""
 }
 
-// MARK: - Add copy Colosseum assets to scripts
+// MARK: - Copy Colosseum assets to scripts
 
-osxCompiler += "\necho \"Copying Colosseum Tool Assets. This may take a while...\"\n"
+var osxColoCompiler = "#!/bin/bash\ncd -- \"$(dirname \"$0\")\" #changes the terminal's directory to the directory where the script was launched\necho \"Copying Colosseum Tool Assets. This may take a while...\"\n"
 + "mkdir " + ColoAssetsSubFolder + "\n"
 + "mkdir " + ColoAssetsFolder + "\n"
 + "mkdir " + ColoWiimmsAssetsFolder + "\n"
 
-windowsCompiler += "\necho \"Copying Colosseum Tool Assets. This may take a while...\"\n"
+var windowsColoCompiler = "set SWIFTFLAGS=-sdk %SDKROOT% -resource-dir %SDKROOT%\\usr\\lib\\swift -I %SDKROOT%\\usr\\lib\\swift -L %SDKROOT%\\usr\\lib\\swift\\windows\necho \"Copying Colosseum Tool Assets. This may take a while...\"\n"
 + "mkdir " + ColoAssetsSubFolder.replacingOccurrences(of: "/", with: "\\") + "\n"
 + "mkdir " + ColoAssetsFolder.replacingOccurrences(of: "/", with: "\\") + "\n"
 + "mkdir " + ColoWiimmsAssetsFolder.replacingOccurrences(of: "/", with: "\\") + "\n"
 
 for asset in ColoAssets {
-	osxCompiler += "cp ../\(asset.path.replacingOccurrences(of: " ", with: "\\ ")) \(ColoAssetsFolder)/\(asset.filename.replacingOccurrences(of: " ", with: "\\ "))\n"
-	windowsCompiler += "copy \"..\\\(asset.path.replacingOccurrences(of: "/", with: "\\"))\" \"\((ColoAssetsFolder + "/" + asset.filename).replacingOccurrences(of: "/", with: "\\"))\"\n"
+	osxColoCompiler += "cp ../\(asset.path.replacingOccurrences(of: " ", with: "\\ ")) \(ColoAssetsFolder)/\(asset.filename.replacingOccurrences(of: " ", with: "\\ "))\n"
+	windowsColoCompiler += "copy \"..\\\(asset.path.replacingOccurrences(of: "/", with: "\\"))\" \"\((ColoAssetsFolder + "/" + asset.filename).replacingOccurrences(of: "/", with: "\\"))\"\n"
 }
-osxCompiler += "cp ../tools/OSX/wiimm/* \(ColoWiimmsAssetsFolder)\n"
-windowsCompiler += "copy ..\\tools\\Windows\\wiimm\\* \((ColoWiimmsAssetsFolder).replacingOccurrences(of: "/", with: "\\"))\"\n"
+osxColoCompiler += "cp ../tools/OSX/wiimm/* \(ColoWiimmsAssetsFolder)\n"
+windowsColoCompiler += "copy ..\\tools\\Windows\\wiimm\\* \((ColoWiimmsAssetsFolder).replacingOccurrences(of: "/", with: "\\"))\"\n"
 
 // MARK: - Add Colosseum swiftc compiler to scripts
-osxCompiler += "echo \"Compiling Colosseum Tool. This may take a while...\"\nswiftc -emit-executable -DENV_OSX -o ./out/Colosseum\\ Tool\\ CLI ../extensions/OSXExtensions.swift"
-windowsCompiler += "\necho \"Compiling Colosseum Tool. This may take a while...\"\nswiftc %SWIFTFLAGS% -emit-executable -DENV_WINDOWS -o .\\out\\Colosseum-Tool.exe ..\\extensions\\WindowsExtensions.swift"
+osxColoCompiler += "echo \"Compiling Colosseum Tool. This may take a while...\"\nswiftc -emit-executable -DENV_OSX -o ./out/Colosseum\\ Tool\\ CLI ../extensions/OSXExtensions.swift"
+windowsColoCompiler += "\necho \"Compiling Colosseum Tool. This may take a while...\"\nswiftc %SWIFTFLAGS% -emit-executable -DENV_WINDOWS -o .\\out\\Colosseum-Tool.exe ..\\extensions\\WindowsExtensions.swift"
 
 for file in ColoSources {
-	osxCompiler += " ../" + file.replacingOccurrences(of: " ", with: "\\ ")
-	windowsCompiler += " \"..\\" + file.replacingOccurrences(of: "/", with: "\\") + "\""
+	osxColoCompiler += " ../" + file.replacingOccurrences(of: " ", with: "\\ ")
+	windowsColoCompiler += " \"..\\" + file.replacingOccurrences(of: "/", with: "\\") + "\""
 }
 windowsCompiler += "\nPAUSE"
+windowsColoCompiler += "\nPAUSE"
 
 // MARK: - Modify OSX script for Linux
 
 var linuxCompiler = osxCompiler.replacingOccurrences(of: "-DENV_OSX", with: "-DENV_LINUX")
 linuxCompiler = linuxCompiler.replacingOccurrences(of: "../extensions/OSXExtensions.swift", with: "../extensions/LinuxExtensions.swift")
 linuxCompiler = linuxCompiler.replacingOccurrences(of: "/tools/OSX/wiimm", with: "/tools/Linux/wiimm")
+var linuxColoCompiler = osxColoCompiler.replacingOccurrences(of: "-DENV_OSX", with: "-DENV_LINUX")
+linuxColoCompiler = linuxColoCompiler.replacingOccurrences(of: "../extensions/OSXExtensions.swift", with: "../extensions/LinuxExtensions.swift")
+linuxColoCompiler = linuxColoCompiler.replacingOccurrences(of: "/tools/OSX/wiimm", with: "/tools/Linux/wiimm")
 
 // MARK: - Write scripts to disk
 
 let osxCompilerPath = outputPath + "/" + "GoD_Tool_OSX_Compiler.sh"
 let linuxCompilerPath = outputPath + "/" + "GoD_Tool_Linux_Compiler.sh"
 let windowsCompilerPath = outputPath + "/" + "GoD_Tool_Windows_Compiler.bat"
+let osxColoCompilerPath = outputPath + "/" + "Colosseum_Tool_OSX_Compiler.sh"
+let linuxColoCompilerPath = outputPath + "/" + "Colosseum_Tool_Linux_Compiler.sh"
+let windowsColoCompilerPath = outputPath + "/" + "Colosseum_Tool_Windows_Compiler.bat"
 
 let osxData = osxCompiler.data(using: .utf8)!
 let linuxData = linuxCompiler.data(using: .utf8)!
 let windowsData = windowsCompiler.data(using: .utf8)!
+let osxColoData = osxColoCompiler.data(using: .utf8)!
+let linuxColoData = linuxColoCompiler.data(using: .utf8)!
+let windowsColoData = windowsColoCompiler.data(using: .utf8)!
 
 do {
 	try osxData.write(to: URL(fileURLWithPath: osxCompilerPath), options: [.atomic])
-	print("Successfully wrote OSX compiler script")
+	print("Successfully wrote XD OSX compiler script")
 } catch {
-	print("Failed to write OSX compiler script")
+	print("Failed to write XD OSX compiler script")
+}
+
+do {
+	try osxColoData.write(to: URL(fileURLWithPath: osxColoCompilerPath), options: [.atomic])
+	print("Successfully wrote Colosseum OSX compiler script")
+} catch {
+	print("Failed to write Colosseum OSX compiler script")
 }
 
 do {
 	try linuxData.write(to: URL(fileURLWithPath: linuxCompilerPath), options: [.atomic])
-	print("Successfully wrote LINUX compiler script")
+	print("Successfully wrote XD LINUX compiler script")
 } catch {
-	print("Failed to write LINUX compiler script")
+	print("Failed to write XD LINUX compiler script")
+}
+
+do {
+	try linuxColoData.write(to: URL(fileURLWithPath: linuxColoCompilerPath), options: [.atomic])
+	print("Successfully wrote Colosseum LINUX compiler script")
+} catch {
+	print("Failed to write Colosseum LINUX compiler script")
 }
 
 do {
 	try windowsData.write(to: URL(fileURLWithPath: windowsCompilerPath), options: [.atomic])
-	print("Successfully wrote Windows compiler script")
+	print("Successfully wrote XD Windows compiler script")
 } catch {
-	print("Failed to write Windows compiler script")
+	print("Failed to write XD Windows compiler script")
+}
+
+do {
+	try windowsColoData.write(to: URL(fileURLWithPath: windowsColoCompilerPath), options: [.atomic])
+	print("Successfully wrote Colosseum Windows compiler script")
+} catch {
+	print("Failed to write Colosseum Windows compiler script")
 }
--- Colosseum-CLI/main.swift
+++ Colosseum-CLI/main.swift
@@ -116,7 +116,7 @@ func applyPatches() {
 
 	while true {
 		let input = readInput(prompt)
-		guard let index = input.integerValue, index >= 0, index - 1 < patches.count else {
+		guard let index = input.integerValue, index >= 0, index <= patches.count else {
 			printg("Invalid option:", input)
 			continue
 		}
@@ -125,6 +125,50 @@ func applyPatches() {
 		}
 		let patch = patches[index - 1]
 		XGDolPatcher.applyPatch(patch)
+	}
+}
+
+func randomiser() {
+	var options = [
+		"1: Randomise Trainer/Wild Pokemon",
+		"2: Randomise Moves",
+		"3: Randomise Pokemon Types",
+		"4: Randomise Abilities",
+		"5: Randomise Pokemon Base Stats",
+		"6: Randomise Evolutions",
+		"7: Randomise Move Types",
+		"8: Randomise TM Moves",
+	]
+	if game == .XD {
+		options += ["9: Randomise Battle Bingo"]
+	}
+
+	var prompt = "Select a patch to apply:\n\n0: exit\n"
+	options.forEach { (option) in
+		prompt += option + "\n"
+	}
+
+	while true {
+		let input = readInput(prompt)
+		guard let index = input.integerValue, index >= 0, index <= options.count else {
+			printg("Invalid option:", input)
+			continue
+		}
+
+		switch index {
+		case 0: return
+		case 1: XGRandomiser.randomisePokemon()
+		case 2: XGRandomiser.randomiseMoves()
+		case 3: XGRandomiser.randomiseTypes()
+		case 4: XGRandomiser.randomiseAbilities()
+		case 5: XGRandomiser.randomisePokemonStats()
+		case 6: XGRandomiser.randomiseEvolutions()
+		case 7: XGRandomiser.randomiseMoveTypes()
+		case 8: XGRandomiser.randomiseTMs()
+		case 9: if game == .XD { XGRandomiser.randomiseBattleBingo() }
+		default: printg("Invalid option:", input); continue
+		}
+		printg("Don't forget to rebuild the ISO in the main menu once you're done")
 	}
 }
 
@@ -138,13 +182,14 @@ func mainMenu() {
 		2: List files - List all files in the ISO
 		3: Import/Export files - Use this to export files for manual editing and to reimport them
 		4: Patches - Pick some useful patches to apply to the game
+		5: Randomiser - Select options for randomising the game (rebuild ISO after)
 		"""
 		if game == .XD {
 			prompt += """
 
-			5: Encode data tables - Write all data to json files that can be edited
-			6: Decode data tables - Import the edited json files for the data tables
-			7: Document data - Dumps all information about the game into text files for reference only
+			6: Encode data tables - Write all data to json files that can be edited
+			7: Decode data tables - Import the edited json files for the data tables
+			8: Document data - Dumps all information about the game into text files for reference only
 			"""
 		}
 
@@ -156,9 +201,10 @@ func mainMenu() {
 		case "2": listFiles()
 		case "3": importExportFiles()
 		case "4": applyPatches()
-		case "5": if game == .XD { XGUtility.encodeISO() }
-		case "6": if game == .XD { XGUtility.decodeISO() }
-		case "7": if game == .XD { XGUtility.documentISO() }
+		case "5": randomiser()
+		case "6": if game == .XD { XGUtility.encodeISO() }
+		case "7": if game == .XD { XGUtility.decodeISO() }
+		case "8": if game == .XD { XGUtility.documentISO() }
 		default: invalidOption(input)
 		}
 	}
@@ -187,3 +233,4 @@ func main() {
 main()
 printg("Press enter to close the tool...")
 _ = readLine() // pause at end of execution
+
