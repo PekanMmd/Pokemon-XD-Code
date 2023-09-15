e4c3a5098ab3235ab79d3f74e5eea3d94b281e49
Stars Momodu <starsmomodu@gmail.com>
2023-09-15 18:04:03 +0100

    meta changes

--- Revolution Tool CL/main.swift
+++ Revolution Tool CL/main.swift
@@ -10,21 +10,3 @@ import Foundation
 ToolProcess.loadISO(exitOnFailure: true)
 
 
-
-//XGPatcher.reimplementHardcodedNameIDsForPokemonEnum()
-//XGUtility.importFileToISO(.dol)
-//XGISO.current.save()
-
-//for i in 0 ... 5 {
-//	let rentalMon = XGTrainerPokemon(index: i, file: rentalPassDeck)
-//	rentalMon.species = .index(494 + i)
-//	rentalMon.save()
-//}
-//
-//XGPatcher.increasePokemonTotal(by: 10)
-//XGUtility.compileMainFiles()
-
-//PDADumper.dumpData()
-//PDADumper.dumpFiles(writeTextures: false)
-//PDADumper.dumpMSG()
-
