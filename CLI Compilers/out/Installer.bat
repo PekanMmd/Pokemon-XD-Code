92342dcb27ed5857b953923d9f1f31fa58fec89a
Stars Momodu <starsmomodu@gmail.com>
2023-09-25 12:07:13 +0100

    update windows installer script

--- CLI Compilers/out/Installer.bat
+++ CLI Compilers/out/Installer.bat
@@ -1,7 +1,8 @@
 IF exist C:\Library\Developer\Platforms\Windows.platform\Developer\SDKs\Windows.sdk\usr\lib\swift ( rmdir /s C:\Library\Developer\Platforms\Windows.platform\Developer\SDKs\Windows.sdk\usr\lib\swift )
+IF exist C:\Library\Developer\Platforms\Windows.platform\Developer\Library\XCTest-development\usr\lib\swift ( rmdir /s C:\Library\Developer\Platforms\Windows.platform\Developer\Library\XCTest-development\usr\lib\swift )
 
 :: Swift setup
-.\Assets\swift\swift-5.6.2-RELEASE-windows10.exe
+.\Assets\swift\swift-5.8.1-RELEASE-windows10.exe
 
 :: Cygwin setup
 .\Assets\cygwin\setup-x86_64.exe -q -n -N
